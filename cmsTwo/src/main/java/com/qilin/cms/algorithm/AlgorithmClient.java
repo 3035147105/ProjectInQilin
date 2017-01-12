0c695d5448ed05407b5f4e348f7dc3d7a27ce74c
gaohaiqing <haiqinggao@qilin99.com>
2017-01-12 19:01:08 +0800

    锁整理，新建算法包，用于存放以后写的算法

---
+++ cmsTwo/src/main/java/com/qilin/cms/algorithm/AlgorithmClient.java
@@ -0,0 +1,61 @@
+package com.qilin.cms.algorithm;
+
+/**
+ * Created by gaohaiqing on 17-1-12.
+ *
+ * 与算法相关的一些记录
+ */
+public class AlgorithmClient {
+    /**
+     * 简单的取一个数字的个、十、百位
+     * @param number  正整数(三位)
+     *                输出 百位数、十位数、个位数
+     */
+    public void hundred(int number){
+        if(number>999 || number<100){
+            System.out.println("请输入一个三位数");
+            return;
+        }
+        int baiwei = number/100;
+        int shiwei = number%100/10;
+        int gewei = number%100%10;
+        System.out.println("个位："+gewei+"十位："+shiwei+"百位："+baiwei);
+    }
+
+    public void suanfa1(){
+        int result = 0;
+        int i = 10000;
+        while (i<100000){
+            if((i%3==0) && (i%10==6)) {
+                result += 1;
+            }
+            i+=1;
+        }
+        System.out.println("个位数为6,且能被3整除的5位整数共有" + result + "个");
+    }
+
+    /**
+     * 1~100之间的所有质数
+     */
+    public void suanfa2(){
+        for(int i=1; i<=100; i++){
+            boolean a  = true;
+            if(i != 1){
+                for(int n=2;n<i;n++){
+                    if(i%n == 0){
+                        a = false;
+                        break;
+                    }
+                }
+            }
+            if (a){
+                System.out.println(i+"是质数");
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        AlgorithmClient alg = new AlgorithmClient();
+        alg.hundred(3294385);
+    }
+}
--- cmsTwo/src/main/java/com/qilin/cms/multiThread/MyLock.java
+++ cmsTwo/src/main/java/com/qilin/cms/multiThread/MyLock.java
@@ -1,8 +1,8 @@
 package com.qilin.cms.multiThread;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.*;
 
 /**
  * Created by gaohaiqing on 16-10-27.
@@ -10,7 +10,35 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 public class MyLock {
 
     public static void main(String[] args){
-        Lock lock1 = new ReentrantLock();
-//        ReentrantReadWriteLock();
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        lock.lock();
+        lock.unlock();
+        lock.tryLock();
+        try {
+            lock.tryLock(1000L, TimeUnit.SECONDS);
+
+            condition.await();
+            condition.await(1000L, TimeUnit.SECONDS);
+            condition.awaitNanos(2000L);
+            condition.awaitUninterruptibly();
+            condition.awaitUntil(new Date());
+            condition.signal();
+            condition.signalAll();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String amao = "阿猫";//wait 跟 notify是 Object类的方法
+        try {
+            amao.wait();
+            amao.notify();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        Lock readLock = readWriteLock.readLock();//读锁
+        Lock writeLock = readWriteLock.writeLock();//写锁
     }
 }
